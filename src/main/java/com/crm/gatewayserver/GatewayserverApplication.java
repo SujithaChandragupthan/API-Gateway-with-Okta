e142ca0206d1953fddd876761358375875a67098
SujithaChandragupthan <sujigupta16@gmail.com>
2023-12-12 08:03:00 -0800

    API Gateway with Okta

---
+++ src/main/java/com/crm/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,18 @@
+package com.crm.gatewayserver;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	
+}
